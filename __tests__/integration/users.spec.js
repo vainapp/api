20d8e7d5c0db0707159adcb392fa1cc9b887382b
João Melo <jopcmelo@gmail.com>
2022-05-10 19:06:15 -0300

    feat: add signup tests

--- __tests__/integration/users.spec.js
+++ __tests__/integration/users.spec.js
@@ -6,8 +6,9 @@ import app from '../../src/app'
 import factory from '../factories'
 import truncate from '../util/truncate'
 import closeRedisConnection from '../util/closeRedisConnection'
+import AccountVerificationLink from '../../src/app/models/AccountVerificationLink'
 
-describe('/users', () => {
+describe('POST /users', () => {
   beforeEach(async () => {
     await truncate()
   })
@@ -39,6 +40,72 @@ describe('/users', () => {
     }
 
     const response = await request(app).post('/users').send(body)
+
+    expect(response.body).toHaveProperty('id')
+  })
+
+  it('should not allow sign up users with already verified email addresses', async () => {
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+
+    await factory.create('User', {
+      verified: true,
+      email,
+    })
+
+    const body = {
+      name: faker.name.findName(),
+      email,
+      password,
+      passwordConfirmation: password,
+    }
+
+    await request(app).post('/users').send(body).expect(403)
+  })
+
+  it('should generate an unverified AccountVerificationLink for the new user', async () => {
+    const password = faker.internet.password()
+
+    const body = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password,
+      passwordConfirmation: password,
+    }
+
+    const response = await request(app).post('/users').send(body)
+
+    const newAccountVerificationLink = await AccountVerificationLink.findOne({
+      where: {
+        user_id: response.body.id,
+      },
+    })
+
+    expect(newAccountVerificationLink.verified).toBe(false)
+  })
+
+  it('should not allow sign ups with different passwords', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        passwordConfirmation: faker.internet.password(),
+      })
+      .expect(400)
+  })
+
+  it('should allow a second attempt to sign up from an unverified email address', async () => {
+    const user = await factory.create('User')
+    const password = faker.internet.password()
+
+    const response = await request(app).post('/users').send({
+      name: user.name,
+      email: user.email,
+      password,
+      passwordConfirmation: password,
+    })
 
     expect(response.body).toHaveProperty('id')
   })
