87d430ca4395ea78f34d2dd948bc30cb6b2c188b
João Melo <jopcmelo@gmail.com>
2022-05-10 22:05:19 -0300

    feat: cluster the app

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import app from './app'
 
-app.listen(process.env.APP_PORT, () => {
-  console.info('Server is running!')
-  console.log('Environment:', process.env.NODE_ENV)
+const { pid, env } = process
+
+const server = app.listen(env.APP_PORT, () => {
+  console.info(
+    `Server running in process ${pid} | environment: ${env.NODE_ENV}`
+  )
+})
+
+process.on('SIGTERM', () => {
+  console.info('Server ending at', new Date().toISOString())
+  server.close(() => process.exit())
 })
