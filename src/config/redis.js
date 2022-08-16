ecba838fdfd464d4be00b81d8a476a95c73c9c2f
João Melo <jopcmelo@gmail.com>
2022-08-16 02:07:26 -0300

    fix: remove redis password for test env

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,11 @@
 export default {
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
+  ...(process.env.NODE_ENV === 'test'
+    ? {}
+    : {
+        password: process.env.REDIS_PASSWORD,
+      }),
   expirations: {
     staticData: 1 * 60 * 60 * 12, // 12 hours
   },
