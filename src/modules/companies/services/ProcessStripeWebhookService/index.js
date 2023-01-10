297000cd74ca439e1761ec12e87485dd77579d59
João Melo <jopcmelo@gmail.com>
2023-01-10 20:28:57 -0300

    chore: add console.log to test on DigitalOcean

--- src/modules/companies/services/ProcessStripeWebhookService/index.js
+++ src/modules/companies/services/ProcessStripeWebhookService/index.js
@@ -11,24 +11,31 @@ class ProcessStripeWebhookService {
 
     switch (type) {
       case 'payment_intent.succeeded':
+        console.log('payment_intent.succeeded')
         await handlePaymentIntentSucceeded(payload)
         break
       case 'invoice.payment_succeeded':
+        console.log('invoice.payment_succeeded')
         await handleInvoicePaymentSucceeded(payload)
         break
       case 'customer.subscription.deleted':
+        console.log('customer.subscription.deleted')
         await handleCustomerSubscriptionDeleted(payload)
         break
       case 'customer.deleted':
+        console.log('customer.deleted')
         await handleCustomerDeleted(payload)
         break
       case 'invoice.payment_failed':
+        console.log('invoice.payment_failed')
         await handleInvoicePaymentFailed(payload)
         break
       case 'customer.subscription.trial_will_end':
+        console.log('customer.subscription.trial_will_end')
         await handleCustomerSubscriptionTrialWillEnd(payload)
         break
       default:
+        console.log('unhandled webhook type', type)
         break
     }
   }
