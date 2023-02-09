7e6ad45bdf1ade67ebba418d0493bbeef4f7cfde
João Melo <jopcmelo@gmail.com>
2023-02-09 15:47:20 -0300

    fix(email-template): fix forced email colors

--- scripts/ses_sendtemplateemail.js
+++ scripts/ses_sendtemplateemail.js
@@ -6,10 +6,10 @@ AWS.config.update({ region: 'us-east-1' })
 
 buildDirectEmailParams({
   toAddress: 'jopcmelo@gmail.com',
-  template: 'COMPANY_CUSTOMER_SUBSCRIPTION_TRIAL_WILL_END',
+  template: 'COMPANY_VERIFY_EMAIL',
   templateData: {
     name: 'João',
-    description: '1 x Pro (R$60,00 / month)',
+    link: 'https://dev.vaiapp.com.br',
   },
 }).then((params) => {
   let sendPromise = new AWS.SES({ apiVersion: '2010-12-01' })
