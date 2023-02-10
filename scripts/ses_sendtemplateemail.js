770279a8747b73f2cf0397db63a1f470fde7d172
João Melo <jopcmelo@gmail.com>
2023-02-10 14:05:56 -0300

    fix(email-template): fix email templates after testing

--- scripts/ses_sendtemplateemail.js
+++ scripts/ses_sendtemplateemail.js
@@ -6,12 +6,23 @@ AWS.config.update({ region: 'us-east-1' })
 
 buildDirectEmailParams({
   toAddress: 'jopcmelo@gmail.com',
-  template: 'COMPANY_CUSTOMER_SUBSCRIPTION_TRIAL_WILL_END',
+  template: 'USER_VERIFY_EMAIL',
   templateData: {
     name: 'João',
     action_url: 'https://dev.vainapp.com.br',
     close_account_url: 'https://dev.vainapp.com.br',
     feedback_url: 'https://dev.vainapp.com.br',
+    description: '1x de R$60,00 por mês',
+    billing_url: 'https://dev.vainapp.com.br',
+    payment_intent_id: 'pi_1H8QZoJZ2Z2Z2Z2Z2Z2Z2Z2Z',
+    date: '10 de outubro de 2020 às 15:00',
+    subscription_active_until: '10 de outubro de 2020 às 15:00',
+    invitee_sender_name: 'João',
+    invite_sender_organization_name: 'Vain Cortes',
+    login_url: 'https://dev.vainapp.com.br',
+    email: 'jopcmelo@gmail.com',
+    password: '27d1S3cR3t',
+    code: '7238',
   },
 }).then((params) => {
   let sendPromise = new AWS.SES({ apiVersion: '2010-12-01' })
--- scripts/ses_updatetemplate.js
+++ scripts/ses_updatetemplate.js
@@ -1,34 +1,30 @@
-// Load the AWS SDK for Node.js
 let fs = require('fs')
 
 let AWS = require('aws-sdk')
-// Set the region
+
 AWS.config.update({ region: 'us-east-1' })
 
-// Create updateTemplate parameters
 let params = {
   Template: {
-    TemplateName: 'COMPANY_CUSTOMER_SUBSCRIPTION_TRIAL_WILL_END' /* required */,
+    TemplateName: 'USER_VERIFY_EMAIL',
     HtmlPart: fs
       .readFileSync(
-        'src/modules/companies/views/COMPANY_CUSTOMER_SUBSCRIPTION_TRIAL_WILL_END/COMPANY_CUSTOMER_SUBSCRIPTION_TRIAL_WILL_END.html'
+        'src/modules/users/views/USER_VERIFY_EMAIL/USER_VERIFY_EMAIL.html'
       )
       .toString(),
-    SubjectPart: 'Seu período de teste da Vain está acabando!',
+    SubjectPart: 'Verifique seu e-mail',
     TextPart: fs
       .readFileSync(
-        'src/modules/companies/views/COMPANY_CUSTOMER_SUBSCRIPTION_TRIAL_WILL_END/COMPANY_CUSTOMER_SUBSCRIPTION_TRIAL_WILL_END.txt'
+        'src/modules/users/views/USER_VERIFY_EMAIL/USER_VERIFY_EMAIL.txt'
       )
       .toString(),
   },
 }
 
-// Create the promise and SES service object
 let templatePromise = new AWS.SES({ apiVersion: '2010-12-01' })
   .updateTemplate(params)
   .promise()
 
-// Handle promise's fulfilled/rejected states
 templatePromise
   .then(() => {
     console.log('Template Updated')
